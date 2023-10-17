70fd17d3df902c75955c22c746b97734499aee50
danielbatres <danielbatresuser@gmail.com>
2023-10-16 19:42:09 -0600

    Inheritance

--- constructor.js
+++ constructor.js
@@ -12,7 +12,35 @@ var person = new Person('Alex')
 console.log(person.name)
 console.log(person.greet())
 
+// inherit class
+
+function Developer(name) {
+  this.name = name;
+}
+
+Developer.prototype = Object.create(Person.prototype)
+Developer.prototype.writeCode = function (coffee) {
+  if (coffee) console.log('I am working in a new feature');
+  else console.log('I need coffee, please!');
+}
+
+var developer = new Developer('Alex')
+developer.greet()
+developer.writeCode() 
+
 // With ES6
+
+/* 
+  class Developer extends Person {
+    constructor(name) {
+      super(name)
+    }
+
+    writeCode(params) {
+      ...code
+    }
+  }
+*/
 
 class Cat {
   constructor(cat) {
