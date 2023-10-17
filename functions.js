1d87d5b1f23b756ad62dfcb664e8d6ca6fe07719
danielbatres <danielbatresuser@gmail.com>
2023-10-16 18:55:44 -0600

    Function declaration and expression

---
+++ functions.js
@@ -0,0 +1,15 @@
+// Function declaration
+
+function getResult() {
+  return 'Results'
+}
+
+getResult() //> return Results
+
+// Function expression
+
+const getResults = function () {
+  return 'Results'
+}
+
+getResults()
