be40f425ea8d44d522cb4f0f360238937e3792e1
danielbatres <danielbatresuser@gmail.com>
2023-10-16 19:35:42 -0600

    Classes, constructors and methods

---
+++ constructor.js
@@ -0,0 +1,29 @@
+// Before ES6
+
+function Person(name) {
+  this.name = name;
+}
+
+Person.prototype.greet = function () {
+  return 'Hello I am ' + this.name;
+}
+
+var person = new Person('Alex')
+console.log(person.name)
+console.log(person.greet())
+
+// With ES6
+
+class Cat {
+  constructor(cat) {
+    this.name = cat
+  }
+
+  greet() {
+    return 'Hello I am a cat and my name is ' + this.name;
+  }
+}
+
+const cat = new Cat('Oliver')
+console.log(cat.name)
+console.log(cat.greet())
